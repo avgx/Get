6b430d3d3ddf05d17b20a5f23bb6c61d68bf23df
avgx <agovorovsky@gmail.com>
2026-05-23 23:48:22 +0300

    paged response

---
+++ Sources/HTTP/HTTPClient+pages.swift
@@ -0,0 +1,23 @@
+import Foundation
+import EncodeDecode
+import RequestResponse
+
+extension HTTPClient {
+    func pages<T: Decodable & Sendable>(
+        _ request: Request<PagedResponse<T>>,
+        with builder: RequestBuilder,
+        decoder: JSONDecoder = JSONDecoder()
+    ) async throws -> [T] {
+        let res = try await data(for: request, with: builder)
+        let ct = (res.response as? HTTPURLResponse)?
+            .value(forHTTPHeaderField: "Content-Type") ?? ""
+
+        if ct.hasPrefix("text/event-stream") {
+            return try decodeSse(T.self, from: res.data, using: decoder)
+        }
+        if isMultipartRelated(ct) {
+            return try decodeMultipartRelated(T.self, contentType: ct, from: res.data, using: decoder)
+        }
+        throw URLError(.cannotDecodeContentData)
+    }
+}
