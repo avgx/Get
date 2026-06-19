0ec31c8e9c49a5c6db1f2c96955c90b0fb691bc2
avgx <agovorovsky@gmail.com>
2026-06-19 16:37:42 +0300

    return back HttpClient5

---
+++ Sources/Get/HttpClient5Compatibility.swift
@@ -0,0 +1,57 @@
+import Foundation
+import SSLPinning
+
+extension URLSessionConfiguration {
+    public static var custom: URLSessionConfiguration {
+        let x: URLSessionConfiguration = .ephemeral
+        x.timeoutIntervalForRequest = 10
+        x.timeoutIntervalForResource = 30
+        x.requestCachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        return x
+    }
+}
+
+extension JSONEncoder {
+    public static var custom: JSONEncoder {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.dataEncodingStrategy = .base64
+        encoder.outputFormatting = [.withoutEscapingSlashes, .prettyPrinted]
+        return encoder
+    }
+}
+
+extension JSONDecoder {
+    public static var custom: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        decoder.dataDecodingStrategy = .base64
+        return decoder
+    }
+}
+
+/// TLS policy matching the historical ``HttpClient5`` surface; maps to ``ServerTrustPolicy``.
+public enum SSL: Sendable {
+    case system
+    case trustEveryone
+    case pinning([Pin])
+}
+
+extension SSL {
+    func asServerTrustPolicy() -> ServerTrustPolicy {
+        switch self {
+        case .system: .system
+        case .trustEveryone: .trustEveryone
+        case .pinning(let pins): .pinning(pins)
+        }
+    }
+}
+
+extension HttpClient5 {
+    /// Logging level for the underlying ``HTTPClient`` task logger (SwiftLog); not Pulse-based.
+    public enum LoggerConfiguration: String, Sendable, CaseIterable {
+        case none
+        case sensitive
+        case full
+    }
+}
